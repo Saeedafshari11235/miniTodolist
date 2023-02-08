127531dd15fa328e6cfd5cf5408fa8bef5ebd36b
Saeedafshari11235 <msafshari11235@gmail.com>
2023-02-08 17:58:02 +0330

    adding delete option

--- js/script.js
+++ js/script.js
@@ -1,13 +1,19 @@
 let input = document.querySelector("input")
-// let newTodo = document.createElement("li")
 let ul = document.querySelector("ul")
+let delt = document.querySelectorAll("i")
 input.addEventListener("keydown",(event)=>{
     if(event.key == "Enter"){
         event.preventDefault()
         if(event.target.value){
-            // console.log(event.target.value)
             let newTodo = document.createElement("li")
-            newTodo.innerHTML = "<span>" + event.target.value + "</span><i class=\"fas fa-trash\"></i>"
+            let newTodoSpan = document.createElement("span")
+            newTodoSpan.innerHTML = event.target.value
+            let newTodoTrash = document.createElement("i")
+            newTodoTrash.className = "fas fa-trash"
+            newTodoTrash.addEventListener("click",(event)=>{
+                event.target.parentElement.remove()
+            })
+            newTodo.append(newTodoSpan,newTodoTrash)
             ul.appendChild(newTodo)
             event.target.value = ""
         }
